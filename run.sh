2a7ba2982b866d9fbf642acb9f931731bbcf634a
Steve Robbins <steve@steverobbins.com>
2024-11-04 13:13:44 -0800

    Support multiple cameras and dates in one command

--- run.sh
+++ run.sh
@@ -15,40 +15,44 @@ if [ -z "$1" ]; then
     exitHelp
 fi
 
-CAMERA=$1
-DATE=$2
-RUNTIME=60
+BASE_DIR=$(dirname $(realpath $0))
+DATETIME=`date -u +"%Y%m%d%H%M%S"`
 
+RUNTIME=60
 if [[ $3 != '' ]]; then
     RUNTIME="$3"
 fi
 
-DATETIME=`date -u +"%Y%m%d%H%M%S"`
+IFS=',' read -ra CAMERAS <<< "$1"
+for CAMERA in "${CAMERAS[@]}"; do
+    IFS=',' read -ra DATES <<< "$2"
+    for DATE in "${DATES[@]}"; do
+        mkdir -p "$BASE_DIR/result/$DATETIME/$DATE/$CAMERA" "$BASE_DIR/cache/$DATE/$CAMERA"
+        cd "$BASE_DIR/result/$DATETIME/$DATE/$CAMERA"
 
-mkdir -p "result/$DATETIME" "cache/$DATE/$CAMERA"
-cd "result/$DATETIME"
+        python3.11 $BASE_DIR/prep-images.py $CAMERA $DATE 2>&1 | tee prep.log
 
-python3.11 ../../prep-images.py $CAMERA $DATE 2>&1 | tee prep.log
+        if [ -f ~/bin/command-threader ]; then
+            # This is parallel-ish script on my machine to run commands asyncronously.
+            # Replace with whatever works for you, or download them one at a time
+            #
+            # https://www.gnu.org/software/parallel/parallel_tutorial.html
+            ~/bin/command-threader download-images.sh 25 >> prep.log
+        else
+            bash download-images.sh >> prep.log
+        fi
 
-if [ -f ~/bin/command-threader ]; then
-    # This is parallel-ish script on my machine to run commands asyncronously.
-    # Replace with whatever works for you, or download them one at a time
-    #
-    # https://www.gnu.org/software/parallel/parallel_tutorial.html
-   ~/bin/command-threader download-images.sh 25 >> prep.log
-else
-   bash download-images.sh >> prep.log
-fi
+        cp *.jpg "$BASE_DIR/cache/$DATE/$CAMERA" >> prep.log
 
-cp *.jpg "../../cache/$DATE/$CAMERA" >> prep.log
+        FILE_COUNT=$(ls -l *.jpg | wc -l)
+        FRAME_RATE=$((FILE_COUNT / RUNTIME))
 
-FILE_COUNT=$(ls -l *.jpg | wc -l)
-FRAME_RATE=$((FILE_COUNT / RUNTIME))
+        echo "Found $FILE_COUNT files.  Runtime for $RUNTIME seconds yeilds framerate $FRAME_RATE.  Rendering..."
 
-echo "Found $FILE_COUNT files.  Runtime for $RUNTIME seconds yeilds framerate $FRAME_RATE.  Rendering..."
+        ffmpeg -framerate "$FRAME_RATE" -pattern_type glob -i '*.jpg' video.mp4 | tee prep.log
 
-ffmpeg -framerate "$FRAME_RATE" -pattern_type glob -i '*.jpg' video.mp4
+        rm -rf *.jpg
 
-rm -rf *.jpg
-
-cp video.mp4 ~/Downloads/$CAMERA-$DATETIME.mp4
+        cp video.mp4 ~/Downloads/$CAMERA-$DATE-$DATETIME.mp4
+    done
+done
